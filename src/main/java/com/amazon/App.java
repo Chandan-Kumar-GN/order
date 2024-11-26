e075a7c7a52a14476a4312e2c5367cc7bf08c51a
Chandan Kumar G N <Chandankumargn16@gmail.com>
2024-11-26 14:04:33 +0530

    Update App.java

--- src/main/java/com/amazon/App.java
+++ src/main/java/com/amazon/App.java
@@ -6,5 +6,6 @@ package com.amazon;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Java");
     }
 }
